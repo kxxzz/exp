57b7fc72ef178d625ee820402d9b8d54b0c43cc6
www <www@www.com>
2019-01-18 19:44:12 +0800

    x

--- exp_exec.c
+++ exp_exec.c
@@ -10,8 +10,10 @@ enum
 
 typedef struct EXP_ExecDef
 {
-    EXP_Node key;
+    const char* key;
     EXP_Node val;
+    u32 numArgs;
+    EXP_Node args[EXP_ExecDefArgs_MAX];
 } EXP_ExecDef;
 
 typedef vec_t(EXP_ExecDef) EXP_ExecDefMap;
@@ -22,9 +24,17 @@ typedef struct EXP_ExecContext
     EXP_Space* space;
     bool hasHalt;
     int retValue;
-    EXP_ExecDefMap defmap;
+    EXP_ExecDefMap defMap;
     vec_u32 frameStack;
 } EXP_ExecContext;
+
+
+static void EXP_execContextFree(EXP_ExecContext* ctx)
+{
+    vec_free(&ctx->frameStack);
+    vec_free(&ctx->defMap);
+}
+
 
 
 typedef enum EXP_PrimExpType
@@ -116,15 +126,91 @@ static EXP_PrimExpType EXP_getPrimExpType(EXP_Space* space, const char* expHead)
 
 
 
-
-
-static EXP_Node EXP_getMatchedDef(EXP_ExecContext* ctx, const char* expHead)
+static bool EXP_execCheckExp(EXP_Space* space, EXP_Node exp)
 {
-    EXP_Node node = { EXP_NodeInvalidId };
-
-    return node;
+    if (!EXP_isExp(space, exp))
+    {
+        return false;
+    }
+    u32 len = EXP_expLen(space, exp);
+    if (!len)
+    {
+        return false;
+    }
+    EXP_Node* elms = EXP_expElm(space, exp);
+    if (!EXP_isStr(space, elms[0]))
+    {
+        return false;
+    }
+    return true;
 }
 
+static bool EXP_execCheckDefPat(EXP_Space* space, EXP_Node pat)
+{
+    if (!EXP_isExp(space, pat))
+    {
+        return false;
+    }
+    u32 len = EXP_expLen(space, pat);
+    if (!len)
+    {
+        return false;
+    }
+    EXP_Node* elms = EXP_expElm(space, pat);
+    if (!EXP_isStr(space, elms[0]))
+    {
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+static void EXP_execLoadDef(EXP_ExecContext* ctx, EXP_Node exp)
+{
+    EXP_Space* space = ctx->space;
+    if (!EXP_execCheckExp(space, exp))
+    {
+        ctx->hasHalt = true;
+        ctx->retValue = EXIT_FAILURE;
+        return;
+    }
+    EXP_Node* elms = EXP_expElm(space, exp);
+    const char* expHead = EXP_strCstr(space, elms[0]);
+    EXP_PrimExpType primType = EXP_getPrimExpType(space, expHead);
+    if (primType != EXP_PrimExpType_Def)
+    {
+        return;
+    }
+    const char* name = NULL;
+    u32 numArgs = 0;
+    EXP_Node* args = NULL;
+    if (EXP_isStr(space, elms[1]))
+    {
+        name = EXP_strCstr(space, elms[1]);
+    }
+    else if (EXP_execCheckDefPat(space, elms[1]))
+    {
+        EXP_Node* pat = EXP_expElm(space, elms[1]);
+        name = EXP_strCstr(space, pat[0]);
+        numArgs = EXP_expLen(space, elms[1]) - 1;
+        args = pat + 1;
+    }
+    else
+    {
+        ctx->hasHalt = true;
+        ctx->retValue = EXIT_FAILURE;
+        return;
+    }
+    EXP_ExecDef def = { name, numArgs };
+    for (u32 i = 0; i < numArgs; ++i)
+    {
+        def.args[i] = args[i];
+    }
+    vec_push(&ctx->defMap, def);
+}
 
 
 
@@ -134,19 +220,37 @@ static void EXP_execExp(EXP_ExecContext* ctx, EXP_Node exp);
 
 static void EXP_execExpList
 (
-    EXP_ExecContext* ctx, u32 len, EXP_Node* exps,
+    EXP_ExecContext* ctx, u32 expsLen, EXP_Node* exps,
     u32 numArgs, EXP_Node argKeys[EXP_ExecDefArgs_MAX], EXP_Node argVals[EXP_ExecDefArgs_MAX]
 )
 {
-    //vec_push(frameStack, level);
+    u32 defMapSize0 = ctx->defMap.length;
+    vec_push(&ctx->frameStack, defMapSize0);
 
-    for (u32 i = 0; i < len; ++i)
+    for (u32 i = 0; i < expsLen; ++i)
     {
-        EXP_execExp(ctx, exps[i]);
-        if (ctx->hasHalt) return;
+        EXP_execLoadDef(ctx, exps[i]);
+        if (ctx->hasHalt)
+        {
+            vec_resize(&ctx->defMap, defMapSize0);
+            vec_pop(&ctx->frameStack);
+            return;
+        }
     }
 
-    //vec_pop(frameStack);
+    for (u32 i = 0; i < expsLen; ++i)
+    {
+        EXP_execExp(ctx, exps[i]);
+        if (ctx->hasHalt)
+        {
+            vec_resize(&ctx->defMap, defMapSize0);
+            vec_pop(&ctx->frameStack);
+            return;
+        }
+    }
+
+    vec_resize(&ctx->defMap, defMapSize0);
+    vec_pop(&ctx->frameStack);
 }
 
 
@@ -177,23 +281,31 @@ static void EXP_execCallDef(EXP_ExecContext* ctx, EXP_Node def, u32 numArgs, EXP
 
 
 
+
+static EXP_Node EXP_getMatchedDef(EXP_ExecContext* ctx, const char* expHead)
+{
+    EXP_Node node = { EXP_NodeInvalidId };
+
+    return node;
+}
+
+
+
+
+
+
+
 static void EXP_execExp(EXP_ExecContext* ctx, EXP_Node exp)
 {
     EXP_Space* space = ctx->space;
-    u32 len = EXP_expLen(space, exp);
-    if (!len)
+    if (!EXP_execCheckExp(space, exp))
     {
         ctx->hasHalt = true;
         ctx->retValue = EXIT_FAILURE;
         return;
     }
     EXP_Node* elms = EXP_expElm(space, exp);
-    if (!EXP_isStr(space, elms[0]))
-    {
-        ctx->hasHalt = true;
-        ctx->retValue = EXIT_FAILURE;
-        return;
-    }
+    u32 len = EXP_expLen(space, exp);
     const char* expHead = EXP_strCstr(space, elms[0]);
     EXP_Node def = EXP_getMatchedDef(ctx, expHead);
     if (def.id != EXP_NodeInvalidId)
@@ -236,6 +348,7 @@ int EXP_exec(EXP_Space* space, EXP_Node root)
     }
     EXP_ExecContext ctx = { space };
     EXP_execExpListBlock(&ctx, root);
+    EXP_execContextFree(&ctx);
     return ctx.retValue;
 }
 
