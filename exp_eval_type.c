c2329135a3039ffc8631dd6dc02a1ee531460b00
www <www@www.com>
2019-02-23 23:16:14 +0800

    x

--- exp_eval_type.c
+++ exp_eval_type.c
@@ -3,6 +3,50 @@
 
 
 
+typedef struct EXP_EvalTypeContext
+{
+    EXP_Space* space;
+} EXP_EvalTypeContext;
+
+
+
+
+
+bool EXP_evalTypeUnifyX(EXP_EvalTypeContext* ctx, EXP_Node a, EXP_Node b)
+{
+    EXP_Space* space = ctx->space;
+    if (EXP_isTok(space, a))
+    {
+        if (EXP_isTok(space, b))
+        {
+            const char* sa = EXP_tokCstr(space, a);
+            const char* sb = EXP_tokCstr(space, b);
+            if (0 == strcmp(sa, sb))
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return EXP_evalTypeUnifyX(ctx, b, a);
+        }
+    }
+    else if (EXP_isSeq(space, b))
+    {
+        assert(EXP_isTok(space, a));
+
+
+    }
+    else
+    {
+        assert(EXP_isTok(space, b));
+    }
+}
+
+
+
+
+
 
 bool EXP_evalTypeMatch(u32 pat, u32 x)
 {
@@ -12,9 +56,6 @@ bool EXP_evalTypeMatch(u32 pat, u32 x)
     }
     return pat == x;
 }
-
-
-
 
 
 bool EXP_evalTypeUnify(u32 a, u32 b, u32* out)
